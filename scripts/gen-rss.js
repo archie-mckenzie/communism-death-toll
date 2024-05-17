9fab38046141cc1357d302c46e5961705fd7f510
archie-mckenzie <archie@generaltranslation.com>
2024-05-17 13:12:46 -0400

    communism death toll

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require("gray-matter");
 
 async function generate() {
   const feed = new RSS({
-    title: "Your Name",
-    site_url: "https://yoursite.com",
-    feed_url: "https://yoursite.com/feed.xml",
+    title: "Communism Death Toll",
+    site_url: "https://communismdeathtoll.com",
+    feed_url: "https://communismdeathtoll.com/feed.xml",
   });
 
   const posts = await fs.readdir(path.join(__dirname, "..", "pages", "posts"));
